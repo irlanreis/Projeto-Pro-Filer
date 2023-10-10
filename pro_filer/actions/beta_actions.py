5dfb7c51c2a38d88175655d779c811ff201996e3
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2023-10-10 12:04:51 -0400

    Feat: Requisito 01

--- pro_filer/actions/beta_actions.py
+++ pro_filer/actions/beta_actions.py
@@ -5,7 +5,9 @@ def show_deepest_file(context):
     if not context["all_files"]:
         print("No files found")
     else:
-        deepest_file = max(context["all_files"], key=len)
+        deepest_file = max(
+            context["all_files"], key=lambda string: len(string.split("/"))
+        )
         print(f"Deepest file: {deepest_file}")
 
 
@@ -14,15 +16,15 @@ def find_file_by_name(context, search_term, case_sensitive=True):
         return []
 
     found_files = []
+    search_term_lower = search_term.lower
 
     for path in context["all_files"]:
         file_name = path.split("/")[-1]
 
-        if not case_sensitive:
-            file_name.lower()
-            search_term.lower()
+        if not case_sensitive and search_term_lower in file_name.lower():
+            found_files.append(path)
 
-        if search_term in file_name:
+        if case_sensitive and search_term in file_name:
             found_files.append(path)
 
     return found_files
