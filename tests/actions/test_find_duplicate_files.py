9d2aed99dc1f5d59d25b3c419e9fb8724e92997b
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2023-10-11 09:56:49 -0400

    Feat: Requisito 06 finished

--- tests/actions/test_find_duplicate_files.py
+++ tests/actions/test_find_duplicate_files.py
@@ -1,1 +1,36 @@
 from pro_filer.actions.main_actions import find_duplicate_files  # NOQA
+import pytest
+
+
+def test_find_duplicate_exception_files():
+    except_context = {"all_files": ["test1.py", "test2.py", "test3.py"]}
+
+    with pytest.raises(ValueError):
+        find_duplicate_files(except_context)
+
+
+def test_find_duplicate_duplicates_files(tmp_path):
+    path_one_output = tmp_path / "test1"
+    path_two_output = tmp_path / "test2"
+    path_three_output = tmp_path / "air.txt"
+    path_one_output.mkdir()
+    path_two_output.mkdir()
+    path_three_output.touch()
+    path_three_output.write_text("test1")
+    example_text_one = path_one_output / "water.txt"
+    example_text_two = path_two_output / "water.txt"
+    example_text_one.touch()
+    example_text_two.touch()
+
+    duplicated_context = {
+        "all_files": [
+            str(example_text_one),
+            str(example_text_two),
+            str(path_three_output),
+        ]
+    }
+
+    resulted = find_duplicate_files(duplicated_context)
+    assert len(resulted) == 1
+    assert "water.txt" in resulted[0][0]
+    assert "water.txt" in resulted[0][1]
